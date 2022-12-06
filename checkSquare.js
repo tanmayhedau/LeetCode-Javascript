681f22e5cf96534d4a219330809292d0fa4f3b1b
tanmayhedau <tanmayhedau619@gmail.com>
2022-12-06 23:27:36 +0530

    questions

--- checkSquare.js
+++ checkSquare.js
@@ -1,18 +1,21 @@
-function checkSquare(arr,arr1){
-    let map ={}, map1={}
-    for(let val of arr){
-        map[val] = (map[val] || 0)+1
+function checkSquare(arr, arr1) {
+  if (arr.length != arr1.length) return false;
+  let map = {},
+    map1 = {};
+  for (let val of arr) {
+    map[val] = (map[val] || 0) + 1;
+  }
+  for (let val of arr1) {
+    map1[val] = (map1[val] || 0) + 1;
+  }
+  let keys = Object.keys(map);
+  let keys1 = Object.keys(map1);
+  for (let i in keys) {
+    if (keys1[i] * keys1[i] != keys[i]) {
+      return false;
     }
-    for(let val of arr1){
-        map[val] = (map[val] || 0)+1
-    }
-    let keys = Object.keys(map)
-    for(let i in keys.length){
-        if(map1[keys[i] * keys[i]] !== map[keys[i]]){
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 }
 
-console.log(checkSquare([5,2,3,6],[36,9,25,4]))
+console.log(checkSquare([36, 1, 25, 4], [5, 1, 6, 2]));
